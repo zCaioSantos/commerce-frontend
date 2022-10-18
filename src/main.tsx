c2f45f205081d9c621b571f73d3187fd2f19777d
zCaioSantos <caio.fernandes72@gmail.com>
2022-10-18 15:56:01 -0300

    feat: add providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from './styles';
+import { ThemeProvider } from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+ <React.StrictMode>
+  <ThemeProvider>
+   <BrowserRouter>
     <App />
-  </React.StrictMode>
-)
+    <GlobalStyle />
+   </BrowserRouter>
+  </ThemeProvider>
+ </React.StrictMode>
+);
+
