2f2ea08b8591e2f96f5ea844bc1ff1eb65fa0443
zCaioSantos <caio.fernandes72@gmail.com>
2022-10-25 20:19:08 -0300

    feat: add prop name in sidebar list

--- src/services/mocks/sidebarItems.tsx
+++ src/services/mocks/sidebarItems.tsx
@@ -3,11 +3,13 @@ import { BsFillArchiveFill, BsPersonFill } from 'react-icons/bs';
 export const sidebarItems = [
  {
   id: '0f485b8c-45a5-11ed-b878-0242ac120002',
+  name: 'Usuários',
   route: '/users',
   icon: <BsPersonFill />,
  },
  {
   id: '0f485b8c-45a5-11ed-b878-0242ac120003',
+  name: 'Produtos',
   route: '/products',
   icon: <BsFillArchiveFill size={30} />,
  },
