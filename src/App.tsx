9acea4b84d7a02a1478fb2bf8b32a6e8ec8686dd
zCaioSantos <caio.fernandes72@gmail.com>
2022-10-26 16:00:11 -0300

    feat: add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { useLocation } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 import { Routers } from './router';
+import { Container, Main } from './styles.app';
 
 export function App() {
- return <Routers />;
+ const location = useLocation();
+ const routes = ['/login', '/esqueci-minha-senha'];
+ const isExternalRoute = routes.includes(location.pathname);
+ return (
+  <Container>
+   {isExternalRoute ? null : <Sidebar />}
+   <Main>
+    {isExternalRoute ? null : <Header />}
+    <Routers />
+   </Main>
+  </Container>
+ );
 }
-
