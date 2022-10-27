30ea62e1d0eb3f0aefa0438df1dd8461b4075093
zCaioSantos <caio.fernandes72@gmail.com>
2022-10-27 19:51:44 -0300

    feat: add form in page dummy

--- src/pages/Dummy/Layout.tsx
+++ src/pages/Dummy/Layout.tsx
@@ -1,13 +1,13 @@
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
+import { FormAdmin } from '../../modules/FormAdmin';
 import { Container } from './styles';
 import { LayoutProps } from './types';
 
 export function Layout({ data }: LayoutProps) {
  return (
   <Container>
-   <Sidebar />
-   <Header />
+   <FormAdmin />
   </Container>
  );
 }
