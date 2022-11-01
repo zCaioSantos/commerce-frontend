4efd17ab13fa037f3cbcd40009cb5216394273d7
Carlos Henrique <ccarloshos2019@gmail.com>
2022-10-31 21:54:41 -0300

    fech: added images and input corectly in tags

--- src/components/LandingPage/Banner/Layout.tsx
+++ src/components/LandingPage/Banner/Layout.tsx
@@ -1,17 +1,16 @@
-import { Container, Sec, Art } from "../Banner/styles";
+import { Banner } from "../../../assets/images"
+import { Sec, Art, Ban } from "../Banner/styles";
 
 export function Layout(){
   return(
-   <Container>
-
     <Sec>
-      <Art>
-        <span>Conheça nossos exclusivos importados!</span>
-        <h2>Aproveite nossas ofertas de novembro black!</h2>
-        <small>Valores únicos para esse momento de copa</small>
-      </Art>
+      <Ban src={Banner}>
+      </Ban>
+        <Art>
+          <span>Conheça nossos exclusivos importados!</span>
+          <h2>Aproveite nossas ofertas de novembro black!</h2>
+          <small>Valores únicos para esse momento de copa</small>
+        </Art>
     </Sec>
-
-   </Container>
   );
 }
--- src/components/LandingPage/Banner/styles.ts
+++ src/components/LandingPage/Banner/styles.ts
@@ -1,31 +1,29 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
- 
-`
-
 export const Sec = styled.div`
   width: 100%;
   height: 650px;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  background-color: black;
-  background-image: url("../../../assets/images/Banner.png");
-  background-size: 1920px 1080px;
-  background-repeat: no-repeat;
-  background-position: center;
     
   small{
     opacity: 50%;
   }
 `
 
+export const Ban = styled.img`
+  width: 100%;
+  height: 100%;
+`
+
+
 export const Art = styled.div`
-  color: gray;
+  color: #FFF;
   height: 86px;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
   margin: 0 5rem;
+  position: absolute;
 `
