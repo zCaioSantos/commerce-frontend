3ec9768bb56bb08a2cf1611f5c5d1233c59aa6c8
zCaioSantos <caio.fernandes72@gmail.com>
2022-11-03 20:48:49 -0300

    style: add ;

--- src/components/LandingPage/Carousel/styles.ts
+++ src/components/LandingPage/Carousel/styles.ts
@@ -1,8 +1,8 @@
-import  styled  from 'styled-components';
+import styled from 'styled-components';
 
 export const Container = styled.div`
  height: 650px;
-` 
+`;
 
 export const Carrossel = styled.div`
  height: 100%;
@@ -11,16 +11,16 @@ export const Carrossel = styled.div`
  display: flex;
  overflow-x: auto;
  scroll-behavior: smooth;
-`
+`;
 
 export const Item = styled.div`
  margin: 5rem 1rem;
-`
+`;
 
 export const Capa = styled.img`
  width: 420px;
  height: 400px;
-`
+`;
 export const Info = styled.div`
  width: auto;
  height: 90px;
@@ -28,19 +28,19 @@ export const Info = styled.div`
  flex-direction: column;
  justify-content: space-evenly;
  margin: 20px;
-`
+`;
 
 export const Desc = styled.div`
  font-size: 20px;
  color: #464646;
-`
+`;
 export const Category = styled.div`
  font-size: 16px;
  font-style: light;
  opacity: 40%;
-`
+`;
 
 export const Price = styled.div`
  font-size: 20px;
  font-weight: 600;
-`
+`;
