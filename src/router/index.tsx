5bb5d94c89ea809d75dea67605a16cd70abf2424
zCaioSantos <caio.fernandes72@gmail.com>
2022-11-06 21:06:19 -0300

    feat: add routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,10 +4,18 @@ import { Dummy } from '../pages/Dummy';
 export function Routers() {
  return (
   <Routes>
-   <Route path="/home" element={<Dummy />} />
-   <Route path="/users" element={<Dummy />} />
-   <Route path="/products" element={<Dummy />} />
-   <Route path="/logout" element={<h1>logout</h1>} />
+   <Route path="/">
+    <Route index element={<Dummy />} />
+    <Route path="produtos">
+     <Route index element={<h1>Produtos</h1>} />
+     <Route path=":id" element={<h1>Detalhes do Produtos</h1>} />
+    </Route>
+    <Route path="logout" element={<h1>logout</h1>} />
+   </Route>
+   <Route path="/dashboard">
+    <Route path="users" element={<Dummy />} />
+   </Route>
+   <Route path="/*" element={<h1>Pagina nao existe</h1>} />
   </Routes>
  );
 }
