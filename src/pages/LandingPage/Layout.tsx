1aef87ed92e9615c4efa88c36c7dff2d4b563ef6
zCaioSantos <caio.fernandes72@gmail.com>
2022-11-06 22:38:59 -0300

    feat: add request get produts

--- src/pages/LandingPage/Layout.tsx
+++ src/pages/LandingPage/Layout.tsx
@@ -3,17 +3,15 @@ import { TopHeader } from '../../components/LandingPage/TopHeader';
 import { Banner } from '../../components/LandingPage/Banner';
 import { Footer } from '../../components/LandingPage/Footer';
 import { Carousel } from '../../components/LandingPage/Carousel';
+import { LayoutProps } from './types';
 
-
-
-
-export function Layout() {
+export function Layout({ data }: LayoutProps) {
  return (
   <div>
    <TopHeader />
    <Header />
    <Banner />
-   <Carousel />
+   <Carousel data={data} />
    <Footer />
   </div>
  );
