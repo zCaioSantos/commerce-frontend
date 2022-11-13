df7294a5f4e9afa25cbc43dfccf56df850af7a1a
zCaioSantos <caio.fernandes72@gmail.com>
2022-11-13 01:28:51 -0300

    feat: add provider query client

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
+import { App } from './App';
+import queryClient from './services/api/queryClient';
 import { GlobalStyle } from './styles';
 import { ThemeProvider } from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
  <React.StrictMode>
-  <ThemeProvider>
-   <BrowserRouter>
-    <App />
-    <GlobalStyle />
-   </BrowserRouter>
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+   <ThemeProvider>
+    <BrowserRouter>
+     <App />
+     <GlobalStyle />
+    </BrowserRouter>
+   </ThemeProvider>
+  </QueryClientProvider>
  </React.StrictMode>
 );
-
