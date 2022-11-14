8575ad7d0c09da7187d7b1daba35de459e2ed030
zCaioSantos <caio.fernandes72@gmail.com>
2022-11-14 01:07:43 -0300

    refactor: cleaning unnecessary code

--- src/components/LandingPage/Carousel/styles.ts
+++ src/components/LandingPage/Carousel/styles.ts
@@ -1,46 +1,10 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
- height: 650px;
+ width: 100%;
 `;
 
-export const Carrossel = styled.div`
- height: 100%;
- margin: 1rem;
- width: 1480px;
+export const Carrossel = styled.ul`
  display: flex;
- overflow-x: auto;
- scroll-behavior: smooth;
-`;
-
-export const Item = styled.div`
- margin: 5rem 1rem;
- cursor: pointer;
-`;
-
-export const Capa = styled.img`
- object-fit: cover;
-`;
-export const Info = styled.div`
- width: auto;
- height: 90px;
- display: flex;
- flex-direction: column;
- justify-content: space-evenly;
- margin: 20px;
-`;
-
-export const Desc = styled.div`
- font-size: 20px;
- color: #464646;
-`;
-export const Category = styled.div`
- font-size: 16px;
- font-style: light;
- opacity: 40%;
-`;
-
-export const Price = styled.div`
- font-size: 20px;
- font-weight: 600;
+ flex-wrap: wrap;
 `;
