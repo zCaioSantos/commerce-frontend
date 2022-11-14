c97c516f4f69c2fa8008833566d26547e9e1dd7a
zCaioSantos <caio.fernandes72@gmail.com>
2022-11-13 23:36:15 -0300

    style: import cleanup

--- src/components/Inputs/InputText/Controller.tsx
+++ src/components/Inputs/InputText/Controller.tsx
@@ -1,8 +1,6 @@
 import { Layout } from './Layout';
 import { ControllerProps } from './type';
 
-// import { useData } from './data';
 export function Controller({ data }: ControllerProps) {
- // const { data } = useData();
  return <Layout data={data} />;
 }
--- src/components/Table/Table/Controller.tsx
+++ src/components/Table/Table/Controller.tsx
@@ -1,7 +1,6 @@
 import { Layout } from './Layout';
-// import { useData } from './data';
 import { ControllerProps } from './types';
+
 export function Controller({ data }: ControllerProps) {
- //  const { data } = useData();
  return <Layout data={data} />;
 }
