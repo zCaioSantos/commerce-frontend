b835da55c3c1773067ede91d191784c35f6d85c8
zCaioSantos <caio.fernandes72@gmail.com>
2022-11-13 23:57:17 -0300

    refactor: cleaning unnecessary code

--- src/components/LandingPage/Footer/Layout.tsx
+++ src/components/LandingPage/Footer/Layout.tsx
@@ -1,19 +1,16 @@
-import { BsFacebook, BsInstagram, BsTelegram, BsTwitter } from "react-icons/bs";
-import { Container, Icons, Div, Sec } from "./styles";
+import { BsFacebook, BsInstagram, BsTelegram, BsTwitter } from 'react-icons/bs';
+import { Container, Icons } from './styles';
 
-
-export function Layout(){
- return(
+export function Layout() {
+ return (
   <Container>
-   <Sec>
-    <Icons>
-     <BsTwitter color="F79F7C" size={36}/>
-     <BsFacebook color="F79F7C" size={36}/>
-     <BsTelegram color="F79F7C" size={36}/>
-     <BsInstagram color="F79F7C" size={36}/>
-    </Icons>
-    <Div>Siga nos também nas redes sociais</Div>
-   </Sec>
+   <Icons>
+    <BsTwitter color="F79F7C" size={36} />
+    <BsFacebook color="F79F7C" size={36} />
+    <BsTelegram color="F79F7C" size={36} />
+    <BsInstagram color="F79F7C" size={36} />
+   </Icons>
+   <span>Siga nos também nas redes sociais</span>
   </Container>
  );
 }
