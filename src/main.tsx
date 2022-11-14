f0b2f06b8860cd181699a1a9cb052d3298c2a5e6
zCaioSantos <caio.fernandes72@gmail.com>
2022-11-13 23:30:37 -0300

    chore: add react toastify

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
+import { App } from './App';
+import queryClient from './services/api/queryClient';
 import { GlobalStyle } from './styles';
 import { ThemeProvider } from './styles/theme';
+import { Slide, ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
  <React.StrictMode>
-  <ThemeProvider>
-   <BrowserRouter>
-    <App />
-    <GlobalStyle />
-   </BrowserRouter>
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+   <ThemeProvider>
+    <BrowserRouter>
+     <App />
+     <GlobalStyle />
+     <ToastContainer
+      position="bottom-right"
+      transition={Slide}
+      closeOnClick={true}
+      limit={3}
+     />
+    </BrowserRouter>
+   </ThemeProvider>
+  </QueryClientProvider>
  </React.StrictMode>
 );
-
