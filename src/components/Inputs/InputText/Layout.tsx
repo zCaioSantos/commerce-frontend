20bb167d559110c4a033f05d95258662a5f522b8
zCaioSantos <caio.fernandes72@gmail.com>
2022-11-18 14:28:50 -0300

    feat: add input value and title

--- src/components/Inputs/InputText/Layout.tsx
+++ src/components/Inputs/InputText/Layout.tsx
@@ -4,9 +4,11 @@ import { LayoutProps } from './type';
 export function Layout({ data }: LayoutProps) {
  return (
   <ContainerInput
+   title={data.placeholder}
    type={data.type}
    placeholder={data.placeholder}
    onChange={(e) => data.getInputValue(e.target.value)}
+   value={data.value}
   />
  );
 }
