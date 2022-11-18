8877f99d7c6c4511666ae361bf7e2f1c26920696
zCaioSantos <caio.fernandes72@gmail.com>
2022-11-18 14:31:25 -0300

    feat: add card

--- src/pages/LandingPage/Controller.tsx
+++ src/pages/LandingPage/Controller.tsx
@@ -4,8 +4,10 @@ import { ResponseProps } from '../../services/types/ResponseProps';
 import { Layout } from './Layout';
 
 export function Controller() {
- const { data, isFetched, isLoading } = useQuery(['getProducts'], () => {
-  const response = axios.get<ResponseProps>('http://localhost:5000/produto/');
+ const { data, isFetched, isLoading } = useQuery(['getProducts'], async () => {
+  const response = await axios.get<ResponseProps>(
+   'http://localhost:5000/produto/'
+  );
   return response;
  });
 
