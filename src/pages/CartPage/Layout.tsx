641930f810f946b822d9e59d8a9224ff0557b6ec
zCaioSantos <caio.fernandes72@gmail.com>
2022-11-21 12:26:45 -0300

    feeat: add list products and value total

--- src/pages/CartPage/Layout.tsx
+++ src/pages/CartPage/Layout.tsx
@@ -1,12 +1,14 @@
+import { Button } from '../../components/Button';
 import { CardToCart } from '../../components/Cards/CardToCart';
+import { HrVertical } from '../../components/HrVertical';
 import { Footer } from '../../components/LandingPage/Footer';
 import { Header } from '../../components/LandingPage/Header';
 import { TopHeader } from '../../components/LandingPage/TopHeader';
 import { useCartShop } from '../../services/context/ShopCart/hook';
-import { Container, Content, ListProducts } from './styles';
+import { CartContainer, Container, Content, ListProducts } from './styles';
 
 export function Layout() {
- const { listCartProducts, getValueTotal } = useCartShop();
+ const { listCartProducts, getValueTotal, getQuantityProducts } = useCartShop();
  return (
   <Container>
    <TopHeader />
@@ -18,12 +20,28 @@ export function Layout() {
       <strong>Você ainda não tem nenhum produto no carrinho..</strong>
      )}
      {listCartProducts.length > 0 && (
-      <>
-       <small>Valor total: R${getValueTotal()}</small>
-       {listCartProducts.map((produto) => (
-        <CardToCart data={produto} />
-       ))}
-      </>
+      <CartContainer>
+       <div className="left">
+        {listCartProducts.map((produto) => (
+         <CardToCart data={produto} />
+        ))}
+       </div>
+       <HrVertical />
+       <div className="right">
+        <strong>
+         Valor total: <span>R${getValueTotal()},00</span>
+        </strong>
+        <strong>
+         Quantidade de produtos: <span>{getQuantityProducts()}</span>
+        </strong>
+        <Button
+         data={{
+          title: 'Comprar',
+          type: 'button',
+         }}
+        />
+       </div>
+      </CartContainer>
      )}
     </ListProducts>
    </Content>
--- src/pages/CartPage/styles.ts
+++ src/pages/CartPage/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.main`
  min-height: 100%;
+ height: 100vh;
  display: flex;
  flex-direction: column;
 `;
@@ -9,7 +10,7 @@ export const Container = styled.main`
 export const Content = styled.article`
  height: 100%;
  flex: 1;
- padding: 1rem 7.8125rem;
+ padding: 5rem 125px;
  overflow-x: hidden;
  background-color: #f6f6f6;
 `;
@@ -30,4 +31,37 @@ export const ContainerLoading = styled.article`
  place-items: center;
 
  padding: 0 7.8125rem;
+`;
+
+export const CartContainer = styled.section`
+ display: flex;
+ gap: 5.375rem;
+ height: 100%;
+
+ .left {
+  flex: 3;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  overflow: hidden;
+  overflow-y: auto;
+ }
+
+ .right {
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+
+  > strong {
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   font-weight: 400;
+   font-size: 24px;
+   padding: 0 0 1.5rem;
+   border-bottom: 1px solid #cccccc;
+  }
+ }
 `;
