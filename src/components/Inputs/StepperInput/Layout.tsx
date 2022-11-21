3a9f2afc7db720e90923f19acfe07d7740889595
zCaioSantos <caio.fernandes72@gmail.com>
2022-11-20 23:50:00 -0300

    feat: add stepper input

---
+++ src/components/Inputs/StepperInput/Layout.tsx
@@ -0,0 +1,22 @@
+import { Button, ContainerInput, Input } from './styles';
+import { LayoutProps } from './types';
+
+export function Layout({ data }: LayoutProps) {
+ return (
+  <ContainerInput>
+   <Button
+    type="button"
+    onClick={() => {
+     // eslint-disable-next-line no-unused-expressions
+     data.value > 1 ? data.onClick(data.value - 1) : '';
+    }}
+   >
+    -
+   </Button>
+   <Input type="number" value={data.value} />
+   <Button type="button" onClick={() => data.onClick(data.value + 1)}>
+    +
+   </Button>
+  </ContainerInput>
+ );
+}
---
+++ src/components/Inputs/StepperInput/styles.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const ContainerInput = styled.div`
+ display: flex;
+ align-items: center;
+`;
+
+export const Button = styled.button`
+ width: 50px;
+ height: 50px;
+
+ display: flex;
+ align-items: center;
+ justify-content: center;
+
+ color: #f79f7c;
+ background-color: #dbdbe4;
+ border-radius: 0.3125rem;
+
+ font-size: 25px;
+
+ transition: all 0.3s ease;
+ &:hover {
+  cursor: pointer;
+  background-color: #464646;
+ }
+`;
+
+export const Input = styled.input`
+ max-height: 3.375rem;
+ font-size: 20px;
+ max-width: 150px;
+ padding: 16px;
+ background-color: transparent;
+ outline: none;
+ text-align: center;
+ &::placeholder {
+  text-align: center;
+ }
+
+ &::-webkit-inner-spin-button,
+ &::-webkit-outer-spin-button {
+  display: none;
+  margin: 0;
+ }
+`;
