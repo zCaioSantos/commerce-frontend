b0b5e5e9afd85c4fb4e0d880f6eaf53adc7a848e
zCaioSantos <caio.fernandes72@gmail.com>
2022-11-23 11:34:20 -0300

    feat: add protect element route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
+import { ProtectedAdmin } from '../Layouts/ProtectedLayout/ProtectedAdmin';
+import { ProtectedEstoquista } from '../Layouts/ProtectedLayout/ProtectedEstoquista';
+import { ProtectedRoute } from '../Layouts/ProtectedLayout/ProtectedRoute';
+
 import { CartPage } from '../pages/CartPage';
 import { Checkout } from '../pages/Checkout';
 import { Dummy } from '../pages/Dummy';
@@ -13,21 +17,28 @@ export function Routers() {
   <Routes>
    <Route path="/">
     <Route index element={<LandingPage />} />
-    <Route path="cart" element={<CartPage />} />
-    <Route path="checkout" element={<Checkout />} />
     <Route path="login" element={<Login />} />
     <Route path="singup" element={<SingUp />} />
+    <Route path="cart" element={<CartPage />} />
+    <Route path="*" element={<h1>Pagina nao existe</h1>} />
     <Route path="produtos">
      <Route index element={<h1>Produtos</h1>} />
      <Route path=":id" element={<ProductDetails />} />
     </Route>
-    <Route path="logout" element={<h1>logout</h1>} />
    </Route>
-   <Route path="/dashboard">
-    <Route path="users" element={<Users />} />
-    <Route path="products" element={<Dummy />} />
+
+   <Route element={<ProtectedRoute />}>
+    <Route path="checkout" element={<Checkout />} />
+
+    <Route path="/dashboard">
+     <Route element={<ProtectedAdmin />}>
+      <Route path="users" element={<Users />} />
+     </Route>
+     <Route element={<ProtectedEstoquista />}>
+      <Route path="products" element={<Dummy />} />
+     </Route>
+    </Route>
    </Route>
-   <Route path="/*" element={<h1>Pagina nao existe</h1>} />
   </Routes>
  );
 }
