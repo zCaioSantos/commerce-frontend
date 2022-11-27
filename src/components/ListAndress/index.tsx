277d7943d8ed76db5e591889eedfc0d617b73a10
zCaioSantos <caio.fernandes72@gmail.com>
2022-11-27 02:09:28 -0300

    feat: add list andress for page profile

---
+++ src/components/ListAndress/index.tsx
@@ -0,0 +1,5 @@
+import { Controller } from './Controller';
+
+(Controller as any).displayName = 'ListAndress';
+
+export { Controller as ListAndress };
---
+++ src/components/ListAndress/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ContainerList = styled.ul`
+ width: 100%;
+ list-style: none;
+
+ display: flex;
+ flex-direction: column;
+ gap: 0.75rem;
+`;
+
+export const Title = styled.legend`
+ font-weight: 400;
+ font-size: 20px;
+ color: #2b3445;
+ margin-bottom: 0.5rem;
+`;
