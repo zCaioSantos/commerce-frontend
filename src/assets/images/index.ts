b9a438e8dfea8870ff52ff7f06a6967d1681c3e4
zCaioSantos <caio.fernandes72@gmail.com>
2022-11-28 15:37:00 -0300

    feat: remove images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,11 +1,4 @@
 import LogoCommerce from './Logo.svg';
 import LogoCommerceSimples from './logoSimples.svg';
-import Iphone from './Iphone.png'
-import Macbook from './Macbook.png';
-import Mouse from './Mouse.png';
-import Tablet from './Tablet.png';
-import AppleWatch from './AppleWatch.png';
-import Banner from './Banner.png';
 
 export { LogoCommerce, LogoCommerceSimples };
-export {Iphone, Macbook, Mouse, Tablet, AppleWatch, Banner};
